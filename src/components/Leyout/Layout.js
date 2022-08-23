b973a3c588cdf8cad2fc34f1b5f1d36867dcf0c1
vlk2022 <volodymyrkostiuk2021@gmail.com>
2022-08-23 23:06:31 +0300

    general

--- src/components/Leyout/Layout.js
+++ src/components/Leyout/Layout.js
@@ -5,12 +5,10 @@ import hero from '../../images/Hero_image.png'
 import './LayoutStyle.css';
 
 
-
 const Layout = () => {
     return (
         <div className={'layout'}>
             <div className={'layout-img'}><img src={hero} alt="foto"/></div>
-
             <div className={'layout-outlet'}><Outlet/></div>
         </div>
     );
--- src/store/store.js
+++ src/store/store.js
@@ -2,10 +2,10 @@ import {configureStore} from "@reduxjs/toolkit";
 
 import userReduser from "./user.slise";
 
+
 const store = configureStore({
     reducer: {
         user: userReduser
     }
-
 });
 export default store;
